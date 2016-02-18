fe66f6615be5597403b0de933aecc3303bfbd7e3
wwwlir <wwwlir@gmail.com>
2016-02-18 13:58:15 +0800

    add

--- z_KeeperFX/src/application/Main.java
+++ z_KeeperFX/src/application/Main.java
@@ -1,27 +1,61 @@
 package application;
 	
+import java.awt.BorderLayout;
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
-	
+	Stage primaryStage;
+	BorderPane rootLayout;
 	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Title");
+		
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			initRootLayout();
+			showMainLayout();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
+//		try {
+//			BorderPane root = new BorderPane();
+//			Scene scene = new Scene(root,400,400);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			primaryStage.setScene(scene);
+//			primaryStage.show();
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	public void initRootLayout() throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("View/RootLayout.fxml"));
+		rootLayout = (BorderPane)loader.load();
+		
+		Scene scene = new Scene(rootLayout);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	public void showMainLayout() throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("View/MainLayout.fxml"));
+		AnchorPane mainLayout = (AnchorPane)loader.load();
+		rootLayout.setCenter(mainLayout);
 	}
 }
